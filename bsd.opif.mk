3c71e6bc2ed0baf203d81f17b94cce3305873807
Fletcher Nichol <fnichol@nichol.ca>
2007-11-22 12:56:11 -0700

    Initial master makefile.

---
+++ bsd.opif.mk
@@ -0,0 +1,58 @@
+# $Id$
+
+# Copyright (c) 2007, Fletcher Nichol
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#
+# Path to common commands
+#
+AWK = /usr/bin/awk
+BASENAME = /usr/bin/basename
+CAT = /bin/cat
+CHMOD = /bin/chmod
+CHROOT = /usr/sbin/chroot
+CP = /bin/cp
+DIRNAME = /usr/bin/dirname
+ECHO = /bin/echo
+EGREP = /usr/bin/egrep
+FIND = /usr/bin/find
+FTP = /usr/bin/ftp
+GREP = /usr/bin/grep
+HEAD = /usr/bin/head
+KSH = /bin/ksh
+LS = /bin/ls
+MD5 = /bin/md5
+MKDIR = /bin/mkdir
+MOUNT_MFS = /sbin/mount_mfs
+MV = /bin/mv
+PATCH_CMD = /usr/bin/patch
+RM = /bin/rm
+SED = /usr/bin/sed
+SORT = /usr/bin/sort
+SUDO = /usr/bin/sudo
+TAR = /bin/tar
+TOUCH = /usr/bin/touch
+UMOUNT = /sbin/umount
+UNAME = /usr/bin/uname
+XARGS = /usr/bin/xargs
+
